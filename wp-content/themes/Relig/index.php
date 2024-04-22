d439a85da27c2b89b9c9c45a1f6e81e77ba15028
Rajiv aryal <rajivary1@gmail.com>
2024-04-22 19:19:11 +0545

    Menus and sidebar

--- wp-content/themes/Relig/index.php
+++ wp-content/themes/Relig/index.php
@@ -1,13 +1,8 @@
-<?php
-/*Theme Name: Religion
-Theme URI: https://aryalrajiv.com.np
-Author: Rajiv Aryal
-Author URI: https://aryalrajiv.com.np
-Description: This is a theme for blog websites. The theme is best for any user tying to make a religious site
-Version: 1.0.0
-License: GNU General Public License v2 or later
-License URI: <https://www.gnu.org/licenses/gpl-2.0.html>
-Text Domain: Rajiv_blog_theme
-Tags: custom-background
-*/
-?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
+  </head>
